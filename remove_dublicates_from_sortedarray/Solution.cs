8dedc69126ff73d16fd678fe26899ea310d63082
Dmytro Petrichenko <dmytro.petrichenko@gmail.com>
2022-10-09 19:03:56 +0100

    remove dublicates form sorted array

---
+++ remove_dublicates_from_sortedarray/Solution.cs
@@ -0,0 +1,9 @@
+﻿public class Solution {
+    public int RemoveDuplicates(int[] nums)
+    {
+       var result = nums.Distinct().ToArray();
+       for (int i = 0; i < result.Length; i++) nums[i] = result[i];
+
+       return result.Length;
+    }
+}
