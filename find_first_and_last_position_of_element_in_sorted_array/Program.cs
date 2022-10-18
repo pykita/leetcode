a0401a3bb52e61875eeba1b1732e20218688ce66
Dmytro Petrichenko <dmytro.petrichenko@gmail.com>
2022-10-18 23:48:52 +0100

    binary search

---
+++ find_first_and_last_position_of_element_in_sorted_array/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Solution {
+    public int[] SearchRange(int[] nums, int target)
+    {
+        if (nums.Length == 0) return new [] { -1, -1 };
+
+        int result = Array.BinarySearch(nums, target);
+        if (result < 0)
+            return new [] { -1, -1 };
+
+        int left = result, right = result;
+
+        while (left - 1 >= 0 && nums[left - 1] == nums[result]) left--;
+
+        while (right + 1 <= nums.Length - 1 && nums[right + 1] == nums[result]) right++;
+
+        return new [] { left, right };
+    }
+}
