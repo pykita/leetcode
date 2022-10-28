f665cc123b1b12abc2f351aca107f5a4c98cd0f3
Dmytro Petrichenko <dmytro.petrichenko@gmail.com>
2022-10-28 17:14:51 +0100

    flatten_2d_vector

---
+++ flatten_2d_vector/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Vector2D
+{
+    private readonly Queue<int> _queue;
+
+    public Vector2D(int[][] vec)
+    {
+        var a = (from arr in vec
+            from i in arr
+            select i).ToArray();
+
+        _queue = new Queue<int>(a);
+    }
+
+    public int Next() => _queue.Dequeue();
+
+    public bool HasNext() => _queue.TryPeek(out int i);
+}
