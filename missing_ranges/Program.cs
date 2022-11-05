dc395b1fd4f5fe52afd42cb4cd6a0e37874cc8e2
Dmytro Petrichenko <dmytro.petrichenko@gmail.com>
2022-11-05 17:49:01 +0000

    mising ranges

---
+++ missing_ranges/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Solution
+{
+    public IList<string> FindMissingRanges(int[] nums, int lower, int upper)
+    {
+        List<string> ans = new List<string>();
+
+        for (int i = 0; i <= nums.Length; i++){
+
+            long start = lower, end = upper;
+            if (i != 0) start = nums[i-1] + 1;
+            if (i != nums.Length) end = nums[i] - 1;
+
+            // если на один болшьше предыдущего равен на один меньше текущего
+            if (start == end) ans.Add(start.ToString());
+            else if (start < end) ans.Add($"{start}->{end}");
+        }
+        return ans;
+    }
+}
