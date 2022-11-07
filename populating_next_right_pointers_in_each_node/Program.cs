c3a286093e62880e0a8cb2d55926bc7b45126474
Dmytro Petrichenko <dmytro.petrichenko@gmail.com>
2022-11-07 21:00:37 +0000

    populate right pointers

---
+++ populating_next_right_pointers_in_each_node/Program.cs
@@ -0,0 +1,48 @@
+﻿public class Solution
+{
+    public Node Connect(Node? root)
+    {
+        var dictionary = new Dictionary<int, Queue<Node>>();
+        AssignNodeIndex(root, 0, dictionary);
+
+        foreach (var kvp in dictionary)
+        {
+            Link(kvp.Value);
+        }
+
+        void Link(Queue<Node> queue)
+        {
+            var prev = queue.Dequeue();
+            while (queue.TryDequeue(out Node? result))
+            {
+                prev.next = result;
+                prev = result;
+            }
+        }
+
+        return root;
+    }
+
+    private void AssignNodeIndex(Node? current, int j, Dictionary<int, Queue<Node>> dictionary)
+    {
+        if (current == null)
+            return;
+
+        if (dictionary.TryGetValue(j + 1, out Queue<Node>? queue))
+        {
+            queue.Enqueue(current);
+        }
+        else
+        {
+            queue = new Queue<Node>();
+            queue.Enqueue(current);
+            dictionary.Add(j + 1, queue);
+        }
+
+        if (current.left != null)
+            AssignNodeIndex(current.left, j + 1, dictionary);
+
+        if (current.right != null)
+            AssignNodeIndex(current.right, j + 1, dictionary);
+    }
+}
