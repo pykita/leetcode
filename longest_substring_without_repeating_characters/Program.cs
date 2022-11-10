db9050034b7b584e3f9e1064fa5b8493a1eb6551
Dmytro Petrichenko <dmytro.petrichenko@gmail.com>
2022-11-10 18:04:20 +0000

    longest substring

---
+++ longest_substring_without_repeating_characters/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Solution {
+    public int LengthOfLongestSubstring(string s)
+    {
+        var q = new Queue<char>();
+        var uniqueSequences = new List<int>();
+
+        foreach (var c in s)
+        {
+            if (q.Contains(c))
+            {
+                uniqueSequences.Add(q.Count);
+                while (q.TryDequeue(out char ch) && ch != c) { }
+            }
+
+            q.Enqueue(c);
+        }
+
+        uniqueSequences.Add(q.Count);
+
+        return uniqueSequences.Max();
+    }
+}
