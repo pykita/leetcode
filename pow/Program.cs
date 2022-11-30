0582080f220557143e1dca4952ac2d3d8a6927c0
Dmytro Petrichenko <dmytro.petrichenko@gmail.com>
2022-11-30 20:58:11 +0000

    pow

---
+++ pow/Program.cs
@@ -0,0 +1,19 @@
+﻿var r = new Solution().MyPow(3, -2);
+
+Console.WriteLine(r);
+
+public class Solution {
+    public double MyPow(double x, int n)
+    {
+        var result = 1.0d ;
+        for (int i = 0; i < Math.Abs(n); i++)
+            result *= x;
+
+        if (n < 0)
+        {
+            result = 1 / result;
+        }
+
+        return result;
+    }
+}
