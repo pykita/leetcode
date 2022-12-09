b0e4e22554cd5b3f0f847faf8d69a3882b50cf66
Dmytro Petrichenko <dmytro.petrichenko@gmail.com>
2022-12-09 01:04:43 +0000

    set zeroes

---
+++ set_matrix_zeroes/Program.cs
@@ -0,0 +1,35 @@
+﻿public class Solution
+{
+    public void SetZeroes(int[][] matrix)
+    {
+        Span<(int, int)> span = stackalloc (int x, int y)[1024];
+        int index = 0;
+        
+        for (int i = 0; i < matrix.Length; i++)
+        {
+            for (int j = 0; j < matrix[i].Length; j++)
+            {
+                if (matrix[i][j] == 0)
+                {
+                    span[index] = (i, j);
+                    index++;
+                }
+            }
+        }
+
+        for (int i = 0; i < index; i++)
+        {
+            var (x, y) = span[i];
+
+            for (int l = 0; l < matrix[x].Length; l++)
+            {
+                matrix[x][l] = 0;
+            }
+
+            for (int n = 0; n < matrix.Length; n++)
+            {
+                matrix[n][y] = 0;
+            }
+        }
+    }
+}
