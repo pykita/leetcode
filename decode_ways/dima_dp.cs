81bfa6f4bee40634273b206ff887271b08530cfe
Dmytro Petrichenko <dmytro.petrichenko@gmail.com>
2023-01-12 13:31:02 +0000

    pp

---
+++ decode_ways/dima_dp.cs
@@ -0,0 +1,61 @@
+﻿public class Solution
+{
+    public int NumDecodings(string s)
+    {
+        if (s[0] == '0')
+            return 0;
+
+        if (s.Length == 1)
+            return 1;
+
+        var dp = new int[s.Length];
+        Array.Fill(dp, -1);
+
+        int next = 0;
+        if (IsLetter(s.Length - 1))
+            dp[s.Length - 2] = 1;
+        else
+            dp[s.Length - 2] = 0;
+        dp[s.Length - 1] = 1;
+
+        for (int j = s.Length - 1, x = s.Length - 2, n = s.Length - 3; x >= 0; j--, x--, n--)
+        {
+            if (!IsLetter(x))
+            {
+                dp[n] = 0;
+                continue;
+            }
+
+            next = 0;
+            if (IsLetter(x))
+                next = dp[x];
+
+            if (IsLetter2(x, j))
+                next = dp[x] + dp[j];
+
+            if (n >= 0)
+                dp[n] = next;
+        }
+
+        return next;
+
+        bool IsLetter2(int start, int end)
+        {
+            if (int.Parse(s.Substring(start, end - start + 1)) > 26)
+                return false;
+
+            return true;
+        }
+
+        bool IsLetter(int start)
+        {
+            if (s[start] == '0')
+                return false;
+
+            if (int.Parse(s.Substring(start, 1)) > 26)
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ decode_ways/dima_enumeration.cs
@@ -0,0 +1,47 @@
+﻿var t1 = new Solution().NumDecodings("10221320");
+var t3 = new Solution().NumDecodings("22");
+var t5 = new Solution().NumDecodings("10");
+
+Console.WriteLine();
+
+public class Solution {
+    public int NumDecodings(string s)
+    {
+        if (s[0] == '0')
+            return 0;
+
+        int i = 0;
+
+        TryDecode(0);
+
+        void TryDecode(int start)
+        {
+            if (start >= s.Length)
+                i++;
+
+            for (int end = start; end < s.Length; end++)
+            {
+                if (!IsLetter(start, end))
+                    continue;
+
+                TryDecode(end + 1);
+            }
+        }
+
+        return i;
+
+        bool IsLetter(int start, int end)
+        {
+            if (end - start >= 2)
+                return false;
+
+            if (int.Parse(s.Substring(start, end - start + 1)) > 26)
+                return false;
+
+            if (end < s.Length - 1 && s[end + 1] == '0')
+                return false;
+
+            return true;
+        }
+    }
+}
