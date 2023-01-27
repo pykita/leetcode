d383a780ee8e50647ff22fa24b26d4eac6feb87e
Dmytro Petrichenko <dmytro.petrichenko@gmail.com>
2023-01-27 12:44:59 +0000

    merge lists

---
+++ merge_k_sorted_lists/dima.cs
@@ -0,0 +1,33 @@
+﻿public class Solution {
+    public ListNode MergeKLists(ListNode[] lists)
+    {
+        if (!lists.Any())
+            return null;
+        
+        var allNumbers = new List<int>();
+        for (int i = 0; i < lists.Length; i++)
+        {
+            while (lists[i] != null)
+            {
+                allNumbers.Add(lists[i] .val);
+                lists[i] = lists[i].next;
+            }
+        }
+
+        if (!allNumbers.Any())
+            return null;
+
+        allNumbers.Sort();
+
+        var root = new ListNode(allNumbers[0]);
+        var current = root;
+
+        for (int i = 1; i < allNumbers.Count; i++)
+        {
+            current.next = new ListNode(allNumbers[i]);
+            current = current.next;
+        }
+
+        return root;
+    }
+}
---
+++ merge_k_sorted_lists/link.txt
@@ -0,0 +1,1 @@
+https://leetcode.com/explore/interview/card/top-interview-questions-hard/117/linked-list/839/
